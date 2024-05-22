ed18a95b760a846cdf1ccf778f694170943bb7da
Moira <moiracorradini@gmail.com>
2024-05-22 17:29:07 +0100

    save

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,51 +3,47 @@ import { useState } from 'react';
 import { Link } from "react-router-dom";
 import '../styles/header.css';
 
-const handleImageClick = () => {
-  console.log('Image clicked');
-  window.location.href = "/";
-};
-
 const Header = () => {
-  const [isHovering, setIsHovering] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
-  const handleMouseOver = () => {
-    setIsHovering(true);
+  const handleImageClick = () => {
+    setIsVisible(!isVisible);
+  };
+  const handleLinkClick = () => {
+    setIsVisible(false); // Hide links when a link is clicked
   };
 
-  const handleMouseOut = () => {
-    setIsHovering(false);
-  };
+
+
   return (
     <nav className="navbar navbar-expand-lg transparent-navbar">
-      <img
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
+      <Link className="nav-link" to="/"  >      <img
+
         className="logo"
         src='./img/logo-colors.png'
         alt='logo'
-        onClick={handleImageClick} />
+        onClick={handleImageClick}
+      /></Link>
+
 
 
       <div className="container-fluid">
-        <Link to="/" aria-current="page" className="navbar-brand"></Link>
+        {/* <Link to="/" aria-current="page" className="navbar-brand"></Link> */}
 
-
-
-        {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button> */}
-        <div className={`nav-links ${isHovering ? 'show' : ''}`} id="navbarNav">
+        <div className={`nav-links ${isVisible ? 'show' : ''}`} id="navbarNav">
           <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/" onClick={handleLinkClick} >Home</Link>
+            </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
+              <Link className="nav-link" to="/about" onClick={handleLinkClick} >About</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/portfolio">Portfolio</Link>
+              <Link className="nav-link" to="/portfolio" onClick={handleLinkClick} >Portfolio</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact</Link>
+              <Link className="nav-link" to="/contact" onClick={handleLinkClick} >Contact</Link>
             </li>
 
           </ul>
